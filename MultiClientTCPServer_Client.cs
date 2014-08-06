6523134ffdf6b1335ca1901936256599f045b37c
DForshner <DForshner@gmail.com>
2014-08-05 20:14:18 -0400

    Added multi client TCP server/client demo

---
+++ MultiClientTCPServer_Client.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+// Simple TCP Server
+
+namespace MultiClientTCPServer_Client
+{
+    public class Program
+    {
+        const int PORT = 8000;
+        const string ADDRESS = "127.0.0.1";
+
+        static void Test(int clientId)
+        {
+            var client = new Client(ADDRESS, PORT);
+    
+            for (var i = 0; i < 100; i++)
+            {
+                client.Send(String.Format("{0} - Test #{0} from client {1}", clientId, i));
+                var recv = client.Receive();
+                Console.WriteLine("{0} - Received: {1}", clientId, recv);
+                Thread.Sleep(100);
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            // Give time for the server to start up when both are in same VS solution.
+            Thread.Sleep(1000);
+
+            const int NUM_CLIENTS = 100;
+
+            // Start thread per client
+            var threads = new List<Thread>(NUM_CLIENTS);
+            for (var i = 0; i < NUM_CLIENTS; i++)
+            {
+                var thread = new Thread(() => Test(i));
+                thread.Start();
+                threads.Add(thread);
+            }
+
+            // Wait for all threads to complete
+            foreach (var thread in threads)
+            {
+                thread.Join();
+            }
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+
+    public class Client
+    {
+        TcpClient client;
+
+        public Client(String address, int portNumber)
+        {
+            this.client = new TcpClient();
+            var ipAddress = IPAddress.Parse(address);
+            var endPoint = new IPEndPoint(ipAddress, portNumber);
+            try
+            {
+                client.Connect(endPoint);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception Occurred: {0}", ex.Message);
+                client.Close();
+            }
+        }
+
+        public void Send(String sendData)
+        {
+            if (sendData == null) { return; }
+            if (!client.Connected) { return; }
+
+            var sendBytes = Encoding.UTF8.GetBytes(sendData);
+            var stream = client.GetStream();
+            stream.Write(sendBytes, 0, sendBytes.Length);
+            stream.Flush();
+        }
+
+        public void Close()
+        {
+            client.Close();
+        }
+
+        public String Receive()
+        {
+            if (!client.Connected) { return null; }
+
+            var stream = client.GetStream();
+            var sb = new StringBuilder();
+            for (var i = 0; i < 10; i++)
+            {
+                if (stream.DataAvailable)
+                {
+                    var buffer = new byte[client.ReceiveBufferSize];
+                    var bytesRead = stream.Read(buffer, 0, buffer.Length);
+                    var recvString = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    Console.WriteLine("-> {0}", recvString);
+                    sb.Append(recvString);
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ MultiClientTCPServer_Server.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+// Simple TCP Server
+// Note: Must run as administrator in visual studio.
+
+namespace MultiClientTCPServer_Server
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            const int PORT = 8000;
+            var server = new Server(PORT, true);
+            server.Start();
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+
+    public class Server 
+    {
+        int connectionAttempts = 0;
+        const string ADDRESS = "127.0.0.1";
+
+        TcpListener listener;
+
+        public Server(int portNumber, bool localHost = false)
+        {
+            var address = (localHost) ? IPAddress.Parse("127.0.0.1") : GetLocalHostAddress();
+            var endpoint = new IPEndPoint(address, portNumber);
+
+            this.listener = new TcpListener(endpoint);
+            Console.WriteLine("Listening on {0}:{1}", address, portNumber); 
+        }
+
+        private IPAddress GetLocalHostAddress()
+        {
+            var name = Dns.GetHostName();
+            var resolvedEntry = Dns.GetHostEntry(name);
+
+            if (resolvedEntry.AddressList.Count() == 0)
+                throw new Exception("Unable to get network addresses for host.");
+
+            foreach (var address in resolvedEntry.AddressList)
+            {
+                if (address.AddressFamily == AddressFamily.InterNetwork)
+                    return address;
+            }
+
+            throw new Exception("Unable to find IPv4 address for host.");
+        }
+
+        public void Start()
+        {
+            this.listener.Start();
+            WaitForConnections();
+        }
+
+        private void WaitForConnections()
+        {
+            var callback = new AsyncCallback(Accept); 
+            this.listener.BeginAcceptTcpClient(callback, new Object());
+        }
+
+        private void Accept(IAsyncResult result)
+        {
+            Interlocked.Increment(ref connectionAttempts);
+            var client = listener.EndAcceptTcpClient(result);
+            Console.WriteLine("Connection: {0} ", client.Client.LocalEndPoint);
+
+            var clientRequest = new RequestHandler(client);
+            clientRequest.Handle();
+        }
+    }
+
+    internal class RequestHandler
+    {
+        TcpClient client;
+
+        public RequestHandler(TcpClient client)
+        {
+            this.client = client;
+        }
+
+        public void Handle()
+        {
+            var stream = client.GetStream();
+
+            while(client.Connected)
+            {
+                if (stream.DataAvailable)
+                {
+                    try
+                    {
+                        var buffer = new byte[client.ReceiveBufferSize];
+                        stream.BeginRead(buffer, 0, buffer.Length, ReadCallBack, buffer);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Exception Occurred: {0}", ex.Message);
+                        stream.Close();
+                        client.Close();
+                    }
+                }
+            }
+
+            Console.WriteLine("Client Disconnected");
+            stream.Close();
+            client.Close();
+        }
+
+        public void ReadCallBack(IAsyncResult result)
+        {
+            var stream = client.GetStream();
+            try
+            {
+                int bytesRead = stream.EndRead(result);
+                if (bytesRead == 0)
+                {
+                    stream.Close();
+                    client.Close();
+                    return;
+                }
+
+                var buffer = result.AsyncState as byte[];
+                var data = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                Console.WriteLine("Received: {0}", data);
+
+                var sendData = DoWork(data);
+                Console.WriteLine("Sent: {0}", sendData);
+
+                var sendBytes = Encoding.UTF8.GetBytes(sendData);
+                stream.Write(sendBytes, 0, sendBytes.Length);
+                stream.Flush();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception Occurred: {0}", ex.Message);
+            }
+        }
+
+        public String DoWork(String str)
+        {
+            return String.Concat("[", str, "] =", str.Length);
+        }
+    }
+}
