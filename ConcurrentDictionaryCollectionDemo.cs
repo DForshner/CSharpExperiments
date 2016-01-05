70408286fb2b88fa837f150d8e9db6fd586de0e0
DForshner <DForshner@gmail.com>
2016-01-04 20:16:13 -0500

    Added some concurrent collection examples

---
+++ ConcurrentDictionaryCollectionDemo.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+// Demo of using concurrent dictionary
+
+namespace ConcurrentDictionaryCollectionDemo
+{
+    public class Program 
+    {
+        static void Main(string[] args)
+        {
+            var toProcess = new ConcurrentQueue<char>();
+            var letterFreq = new ConcurrentDictionary<char, int>();
+            var tasks = new[]
+            {
+                Task.Run(() => ProduceLetters(toProcess)),
+                Task.Run(() => CountFrequencyA(toProcess, letterFreq)),
+                Task.Run(() => CountFrequencyB(toProcess, letterFreq))
+            };
+
+            Task.WaitAll(tasks);
+        }
+
+        static void ProduceLetters(ConcurrentQueue<char> toProcess)
+        {
+            var rnd = new Random(DateTime.UtcNow.Millisecond);
+            const int numLetters = (int)'f' - (int)'a';
+            while(true)
+            {
+                int r = rnd.Next(0, numLetters);
+                var c = (char)((int)'a' + r);
+                toProcess.Enqueue(c);
+            }
+        }
+
+        /// <summary>
+        /// Count frequency using AddOrUpdate and GetOrAdd methods.
+        /// </summary>
+        static void CountFrequencyA(ConcurrentQueue<char> toProcess, ConcurrentDictionary<char, int> freq)
+        {
+            while (true)
+            {
+                char c;
+                if (!toProcess.TryDequeue(out c))
+                {
+                    continue;
+                }
+                int updatedValue = freq.AddOrUpdate(c, 1, (key, oldValue) => oldValue + 1);
+
+                // Other thread could have updated count here
+
+                int currentValue = freq.GetOrAdd(c, 0);
+                if (updatedValue != currentValue)
+                {
+                    DisplayDataRace(c, updatedValue, currentValue);
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Count frequency using Try#### methods
+        /// </summary>
+        static void CountFrequencyB(ConcurrentQueue<char> toProcess, ConcurrentDictionary<char, int> freq)
+        {
+            while (true)
+            {
+                char c;
+                if (!toProcess.TryDequeue(out c))
+                {
+                    continue;
+                }
+
+                // Keep trying to update
+                bool hasUpdated = false;
+                while (!hasUpdated)
+                {
+                    int oldValue;
+                    if (!freq.TryGetValue(c, out oldValue))
+                    {
+                        if (!freq.TryAdd(c, 0))
+                        {
+                            continue; // try to add to dictionary again.
+                        }
+                    }
+
+                    int updatedValue = oldValue + 1;
+                    if (!freq.TryUpdate(c, updatedValue, oldValue))
+                    {
+                        continue; // Try again
+                    }
+
+                    hasUpdated = true;
+
+                    // Other thread could have updated count here
+
+                    int currentValue;
+                    if (!freq.TryGetValue(c, out currentValue))
+                    {
+                        currentValue = 0;
+                    }
+
+                    if (updatedValue != currentValue)
+                    {
+                        DisplayDataRace(c, updatedValue, currentValue);
+                    }
+                }
+            }
+        }
+
+        static object _displayLock = new Object();
+        private static void DisplayDataRace(char c, int updatedValue, int currentValue)
+        {
+            lock (_displayLock)
+            {
+                Console.WriteLine("[{0}] Updated Value: {1} Current Value: {2}", c.ToString(), updatedValue, currentValue);
+            }
+        }
+    }
+}
---
+++ ConcurrentQueueCollectionDemo.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
+using System.Collections.Concurrent;
+
+/// Demo of using concurrent queue collection
+
+namespace ConcurrentQueueCollectionDemo
+{
+    class Program
+    {
+        static void Main()
+        {
+            var toDisplay = new ConcurrentQueue<string>();
+            Task task1 = Task.Run(() => CreateMessages(toDisplay, "ID1"));
+            Task task2 = Task.Run(() => CreateMessages(toDisplay, "ID2"));
+            Task task3 = Task.Run(() => CreateMessages(toDisplay, "ID3"));
+            Task.WaitAll(task1, task2);
+
+            Task producer = Task.Run(() => ProduceMessages(toDisplay, "ID4"));
+            Task consumer = Task.Run(() => ConsumeMessages(toDisplay, "ID4"));
+            Task.WaitAll(producer, consumer);
+        }
+
+        static void CreateMessages(ConcurrentQueue<string> toDisplay, string id)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(5);
+                toDisplay.Enqueue(String.Format("{0}:{1}", id, i));
+            }
+        }
+
+        static void ProduceMessages(ConcurrentQueue<string> toDisplay, string id)
+        {
+            int i = 0;
+            while(true)
+            {
+                Thread.Sleep(5);
+                toDisplay.Enqueue(String.Format("{0}:{1}", id, i));
+                i += 1;
+            }
+        }
+
+        static void ConsumeMessages(ConcurrentQueue<string> toDisplay, string id)
+        {
+            while(true)
+            {
+                Thread.Sleep(1);
+                string msg;
+                if (toDisplay.TryDequeue(out msg))
+                {
+                    Console.WriteLine(msg);
+                }
+            }
+        }
+    }
+
+}
